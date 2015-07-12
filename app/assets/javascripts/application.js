d6628d252790c9cdc107b8fba92bd4977bfd61cd
cmswalker <c.michael.s.walker@gmail.com>
2015-07-11 22:43:14 -0700

    each layer now has a names array

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,13 +20,14 @@
 //= require angular-resource/angular-resource.min.js
 //= require angular-route/angular-route.min.js
 //= require angular-animate/angular-animate.min.js
+//= require angular-bootstrap/ui-bootstrap-tpls.min.js
 
 
 
 
 //JUST IN CASE
 
-var ChartApp = angular.module("ChartApp", ["ngRoute"]);
+var ChartApp = angular.module("ChartApp", ["ngRoute", "ui.bootstrap"]);
 
 ChartApp.config(function($routeProvider) {
 
@@ -83,6 +84,12 @@ ChartApp.controller("ChartCtrl", function ($scope, $http, Chart) {
 				$scope.current_chart.layer_4 = [];
 				$scope.current_chart.layer_5 = [];
 				$scope.current_chart.layer_6 = [];
+				$scope.current_chart.layer_1_names = [];
+				$scope.current_chart.layer_2_names = [];
+				$scope.current_chart.layer_3_names = [];
+				$scope.current_chart.layer_4_names = [];
+				$scope.current_chart.layer_5_names = [];
+				$scope.current_chart.layer_6_names = [];
 
 				var chart_children = [];
 				var start_seconds = new Date().getTime() / 1000;
@@ -95,31 +102,37 @@ ChartApp.controller("ChartCtrl", function ($scope, $http, Chart) {
 						$scope.current_chart.children.forEach(function(v1,i) {
 							//console.log('top layer ', v1.name);
 							chart_children.push(v1.name);
+							$scope.current_chart.layer_1_names.push(v1.name);
 							if (v1.children !== undefined && v1.children.length !== 0 ) {
 								$scope.current_chart.layer_2 = v1.children;
 								//console.log($scope.current_chart.layer_2, "h");
 								v1.children.forEach(function(v2,ii) {
 									chart_children.push(v2.name);
+									$scope.current_chart.layer_2_names.push(v2.name);
 									//console.log('2nd layer ', v2.name);
 									if (v2.children !== undefined && v2.children.length !== 0 ) {
 										$scope.current_chart.layer_3 = v2.children;
 										v2.children.forEach(function(v3,iii) {
 											chart_children.push(v3.name);
+											$scope.current_chart.layer_3_names.push(v3.name);
 											//console.log('3rd layer ', v3.name );
 											if (v3.children !== undefined && v3.children.length !== 0 ) {
 												$scope.current_chart.layer_4 = v3.children;
 												v3.children.forEach(function(v4,iiii) {
 													chart_children.push(v4.name);
+													$scope.current_chart.layer_4_names.push(v4.name);
 													//console.log('4th layer ', v4.name);
 													if (v4.children !== undefined && v4.children.length !== 0) {
 														$scope.current_chart.layer_5 = v4.children;
 														v4.children.forEach(function(v5, i5) {
 															chart_children.push(v5.name);
+															$scope.current_chart.layer_5_names.push(v5.name);
 															//console.log('5th layer ', v5.name);
 															if (v5.children !== undefined && v5.children.length !== 0) {
 																$scope.current_chart.layer_6 = v5.children;
 																v5.children.forEach(function(v6, i6) {
 																	chart_children.push(v6.name);
+																	$scope.current_chart.layer_6_names.push(v6.name);
 																	//console.log('in bottom ', v6.name);
 																})
 															}
@@ -136,7 +149,9 @@ ChartApp.controller("ChartCtrl", function ($scope, $http, Chart) {
 			}
 			//END regen_layer_loop
 
+
 			regen_layer_loop();
+			console.log('yo names ', $scope.current_chart.layer_6_names)
 			
 			var end_seconds = new Date().getTime() / 1000;
 			var total_seconds = end_seconds - start_seconds;
@@ -168,6 +183,8 @@ ChartApp.controller("ChartCtrl", function ($scope, $http, Chart) {
 
 
 
+
+
 	//Initiate the page
 	regenerate();
 
@@ -183,12 +200,14 @@ ChartApp.controller("ChartCtrl", function ($scope, $http, Chart) {
 
 
 
-
-
-
-
-
-
+	  $scope.selected = undefined;
+	  $scope.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 
+	  'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 
+	  'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 
+	  'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 
+	  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 
+	  'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+	
 
 
 
@@ -198,4 +217,6 @@ ChartApp.controller("ChartCtrl", function ($scope, $http, Chart) {
 
 
 });
+
+
 

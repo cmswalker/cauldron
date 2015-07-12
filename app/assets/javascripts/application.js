450f3dad5ee1c00803f0cd887aec0eb62c3ece2c
cmswalker <c.michael.s.walker@gmail.com>
2015-07-11 20:06:27 -0700

    text data bound to searchbox

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require form2js-bower/src/form2js.js
 //= require turbolinks
 //= require D3/d3.min.js
 //= require angular/angular.min.js
@@ -69,11 +70,64 @@ ChartApp.controller("ChartCtrl", function ($scope, $http, Chart) {
 
 	$scope.current_chart = null;
 
-
 	var regenerate = function() {
 		Chart.get().then(
 			function(response){
 				$scope.current_chart = response[0];
+				$scope.current_chart.allchild = null;
+				var chart_children = [];
+				var start_seconds = new Date().getTime() / 1000;
+
+				$scope.current_chart.children.forEach(function(v1,i) {
+					console.log('top layer ', v1.name);
+					chart_children.push(v1.name);
+					if (v1.children !== undefined) {
+						v1.children.forEach(function(v2,ii) {
+							chart_children.push(v2.name);
+							//console.log('2nd layer ', v2.name);
+							if (v2.children !== undefined) {
+								v2.children.forEach(function(v3,iii) {
+									chart_children.push(v3.name);
+									//console.log('3rd layer ', v3.name );
+									if (v3.children !== undefined) {
+										v3.children.forEach(function(v4,iiii) {
+											chart_children.push(v4.name);
+											//console.log('4th layer ', v4.name);
+											if (v4.children !== undefined) {
+												v4.children.forEach(function(v5, i5) {
+													chart_children.push(v5.name);
+													//console.log('5th layer ', v5.name);
+													if (v5.children !== undefined) {
+														v5.children.forEach(function(v6, i6) {
+															chart_children.push(v6.name);
+															//console.log('in bottom ', v6.name);
+														})
+													}
+												});
+											}
+										});
+									}
+								});	
+							}
+						});
+					}
+				});
+			
+			var end_seconds = new Date().getTime() / 1000;
+			var total_seconds = end_seconds - start_seconds;
+			console.log(total_seconds + " seconds for loop");
+			$scope.current_chart.allchild = chart_children;
+
+
+
+
+
+
+
+
+
+
+
 				console.log($scope.current_chart);
 			},
 			function(rejection){
@@ -82,10 +136,22 @@ ChartApp.controller("ChartCtrl", function ($scope, $http, Chart) {
 		);
 	}
 
+	$scope.counter = 0;
+	$scope.change = function() {
+	  $scope.counter++;
+	};
+
+
+
 	//Initiate the page
 	regenerate();
 
 	var ingredient_field = angular.element("#ingredient_field");
+	$scope.second_field = angular.element("#2ndingredient");
+
+
+
+
 
 
 
